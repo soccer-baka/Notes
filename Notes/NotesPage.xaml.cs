14c8f200a94fd66ba9c86a980e33aa9029aa104a
soccer-baka <Hideya@sugiyama-no-iMac.local>
2017-03-06 20:03:50 -0500

    added context actions.

--- Notes/NotesPage.xaml.cs
+++ Notes/NotesPage.xaml.cs
@@ -1,13 +1,28 @@
 ﻿using Xamarin.Forms;
+using System;
+//using PCLStorage;
 
 namespace Notes
 {
     public partial class NotesPage : ContentPage
     {
+        class NoteData
+        {
+            public string Filename { get; set; }
+            public NoteData(string name)
+            {
+                Filename = name;
+            }
+        }
+
         public NotesPage()
         {
             InitializeComponent();
-            string[] temp = { "note 1", "note 2" };
+
+            //var folder = FileSystem.Current.LocalStorage;
+            //var files = folder.GetFilesAsync();
+
+            NoteData[] temp = { new NoteData("test 1"), new NoteData("note 2") };
             listView.ItemsSource = temp;
         }
 
@@ -16,6 +31,12 @@ namespace Notes
             System.Diagnostics.Debug.WriteLine($"{e.SelectedItem} was selected.");
             await Navigation.PushAsync(new Note());
             //listView.SelectedItem = null;
+        }
+
+        public void OnDelete(object sender, EventArgs e)
+        {
+            var menuItem = ((MenuItem)sender);
+            DisplayAlert("Delete Context Action", menuItem.CommandParameter + " delete context action", "OK");
         }
     }
 }
